755ee41c471a80dd1a054df82d9a8db5dbc7c6ff
Barnaby Hutchins <bhutchins@gmail.com>
2021-05-01 18:23:30 +0200

    first commit

---
+++ homework/Week04/Homework.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Week04.Homework where
+
+import Data.Aeson                 (FromJSON, ToJSON)
+import Data.Functor               (void)
+import Data.Text                  (Text, unpack)
+import GHC.Generics               (Generic)
+import Ledger
+import Ledger.Ada                 as Ada
+import Ledger.Constraints         as Constraints
+import Plutus.Contract            as Contract
+import Plutus.Trace.Emulator      as Emulator
+import Wallet.Emulator.Wallet
+
+data PayParams = PayParams
+    { ppRecipient :: PubKeyHash
+    , ppLovelace  :: Integer
+    } deriving (Show, Generic, FromJSON, ToJSON)
+
+type PaySchema = BlockchainActions .\/ Endpoint "pay" PayParams
+
+payContract :: Contract () PaySchema Text ()
+payContract = do
+    pp <- endpoint @"pay"
+    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+    void $ submitTx tx
+    payContract'
+
+payContract' :: Contract () PaySchema Text ()
+payContract'  = Contract.handleError
+    (\err -> Contract.logError $ "Caught error: " ++ unpack err)
+    payContract
+    >> payContract -- This use of >> payContract feels messy
+
+-- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
+-- recipient, but with amounts given by the two arguments. There should be a delay of one slot
+-- after each endpoint call.
+payTrace :: Integer -> Integer -> EmulatorTrace ()
+payTrace x y = do
+    h <- activateContractWallet (Wallet 1) payContract'
+    let
+        recipient = pubKeyHash $ walletPubKey $ Wallet 2
+        toPay1    = PayParams { ppRecipient = recipient
+                              , ppLovelace  = x
+                              } 
+        toPay2    = PayParams { ppRecipient = recipient
+                              , ppLovelace  = y
+                              } 
+
+    callEndpoint @"pay" h toPay1
+    void $ Emulator.waitNSlots 1
+
+    callEndpoint @"pay" h toPay2
+    void $ Emulator.waitNSlots 1
+
+payTest1 :: IO ()
+payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
+
+payTest2 :: IO ()
+payTest2 = runEmulatorTraceIO $ payTrace 1000000000 2000000
